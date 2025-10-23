84e915d21804fe6d1bc16cd327b8b3a2e5cfba26
overist <wj@over.ist>
2025-10-24 03:12:13 +0900

    chore: add @nestjs/swagger, swagger set-up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Points API')
+    .setDescription('The points API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
